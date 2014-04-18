a61b17722546dc215e0930794f0bf10208cafdd2
Logan J Travis <loganjtravis@gmail.com>
2014-04-17 20:01:16 -0700

    Create complete function
    Create complete function. Tested OK with sample output from assignment
    page.

--- assignment01/complete.R
+++ assignment01/complete.R
@@ -12,4 +12,24 @@ complete <- function(directory, id = 1:332) {
     ## ...
     ## where 'id' is the monitor ID number and 'nobs' is the
     ## number of complete cases
+
+    
+    ## initalize data frame for file-id-observation tuples
+    monitors <- data.frame(id, nobs = numeric(length(id)))
+
+    ## populate 'monitors$nobs'
+    ## reads data for each file in 'monitors$id' to 'dataTemp'
+    ## creates a logical vector 'isComplete' where TRUE means
+    ## both sulfate and nitrate are not NA for a row in 'dataTemp'
+    ## updates 'monitor$nobs' with sum('isComplete')
+    for(i in monitors$id) {
+        fileID <- formatC(i, width = 3, format = "d", flag = "0")
+        file <- paste(fileID, ".csv", sep = "")
+        dataTemp <- read.csv(paste(directory, file, sep = "/"))
+        isComplete <- !is.na(dataTemp$nitrate) & !is.na(dataTemp$sulfate)
+        monitors[monitors$id == i, "nobs"] <- sum(isComplete)
+    }
+    
+    ## return 'monitors'
+    monitors
 }
