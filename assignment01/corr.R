a697d901be11af0d5e3ddaa71c5d11d97e440023
Logan J Travis <loganjtravis@gmail.com>
2014-04-18 12:22:32 -0700

    Update corr function comments
    Update corr function comments to correct a typo.

--- assignment01/corr.R
+++ assignment01/corr.R
@@ -5,7 +5,7 @@ corr <- function(directory, threshold = 1) {
     ## 'threshold' is a numeric vector of length 1 indicating the
     ## number of completely observed observations (on all
     ## variables) required to compute the correlation between
-    ## nitrate and sulfate; the default is 0
+    ## nitrate and sulfate; the default is 1
     
     ## Return a numeric vector of correlations
     
