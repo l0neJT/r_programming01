06cb551e2fdaa280c2a3a6840820215e652b48cd
Logan J Travis <loganjtravis@gmail.com>
2014-04-17 20:24:26 -0700

    Create corr function
    Create corr function. Possible rounding error when compared to sample
    output from assignment page.

--- assignment01/corr.R
+++ assignment01/corr.R
@@ -8,4 +8,28 @@ corr <- function(directory, threshold = 0) {
     ## nitrate and sulfate; the default is 0
     
     ## Return a numeric vector of correlations
+    
+    ## initialze numeric vector for storing correlation values
+    correlations <- numeric()
+    
+    ## call 'complete' function on all monitors
+    monitors <- complete("specdata")
+    
+    ## restrict monitors to 'threshold'
+    monitors <- monitors[monitors$nobs >= threshold, ]
+    
+    ## populate 'correlations'
+    ## reads data for each file in 'monitors$id' to 'dataTemp'
+    ## appends correlation of complete nitrate-sulfate
+    ## observations to 'correlations'
+    for(i in monitors$id) {
+        fileID <- formatC(i, width = 3, format = "d", flag = "0")
+        file <- paste(fileID, ".csv", sep = "")
+        dataTemp <- read.csv(paste(directory, file, sep = "/"))
+        corTemp <- cor(dataTemp$nitrate, dataTemp$sulfate, use = "complete.obs")
+        correlations <- c(correlations, corTemp)
+    }
+    
+    ## return 'correlations'
+    correlations
 }
