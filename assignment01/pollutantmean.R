cf13c274d02e6580fdc02fce16fee001d54689c8
Logan J Travis <loganjtravis@gmail.com>
2014-04-17 18:55:55 -0700

    Create pollutantmean function
    Create pollutant mean function. Tested OK with sample output from
    assignment page.

--- assignment01/pollutantmean.R
+++ assignment01/pollutantmean.R
@@ -11,4 +11,27 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     
     ## Return the mean of the pollutant across all monitors list
     ## in the 'id' vector (ignoring NA values)
+    
+    ## initialize character vector for file names
+    files <- as.character()
+    
+    ## initialize numeric vector for pullutant readings
+    readings <- as.numeric()
+    
+    ## populate 'files' from 'id'
+    ## assumes 3 digit file name as CSV
+    for(i in id) {
+        iFile <- formatC(i, width = 3, format = "d", flag = "0")
+        files <- c(files, paste(iFile, ".csv", sep = ""))
+    }
+    
+    ## populate 'readings' from 'files' in 'directory
+    ## reads all data from 'pollutatnt' column
+    for(f in files) {
+        fData <- read.csv(paste(directory, f, sep = "/"))
+        readings <- c(readings, fData[[pollutant]])
+    }
+    
+    ## return mean (ignoring NA values)
+    mean(readings, na.rm = TRUE)
 }
